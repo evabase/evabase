4b4b792fb1cdd692230d713d93d6439374ab8dbb
0xTriple7 <ysqi1024@qq.com>
2022-04-25 16:17:51 +0800

    clear code

--- core/src/interfaces/IEvaFlow.sol
+++ core/src/interfaces/IEvaFlow.sol
@@ -3,8 +3,6 @@
 pragma solidity ^0.8.0;
 
 interface IEvaFlow {
-    function owner() external view returns (address);
-
     function multicall(address target, bytes memory callData) external;
 
     function check(bytes memory checkData)
@@ -13,10 +11,4 @@ interface IEvaFlow {
         returns (bool needExecute, bytes memory executeData);
 
     function execute(bytes memory executeData) external;
-
-    function pause(uint256 flowId, bytes memory extraData) external;
-
-    function start(uint256 flowId, bytes memory extraData) external;
-
-    function destroy(uint256 flowId, bytes memory extraData) external;
 }
